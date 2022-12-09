b7365850b31c9cbf6615a3f06f9e7c8b65cbcd47
Mehdi BENAMAR <MBENAMAR@lfeure.fr>
2022-12-09 14:31:39 +0100

    init projet + vue 'home'

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script>
+  import Logo from "./icons/IconKasa.vue"
+
+  export default {
+    name: "Header",
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-logo">
+      <Logo />
+    </div>
+    <nav class="header-nav">
+      <ul class="header-list">
+        <li>Accueil</li>
+        <li>A propos</li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script>
+import Header from "../components/Header.vue"
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <Header />
+</template>
