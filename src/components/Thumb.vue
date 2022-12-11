f8f3d95afcc19dfa5ec2c61923c16f7924719d94
mehdi ben <test@gmail.comn>
2022-12-11 16:27:56 +0100

    create component logement, create route logement

--- src/components/Thumb.vue
+++ src/components/Thumb.vue
@@ -1,15 +1,15 @@
 <script>
-
-
-export default {
-  name: "Thumb",
-  props: {
-    location: Object
+  export default {
+    name: "Thumb",
+    props: {
+      location: Object
+    }
   }
-}
 </script>
 
 <template>
-      <img :src="location.cover" :alt="location.title" class="thumb-image">
-      <p class="thumb-title">{{ location.title }}</p>
+  <router-link :to="{name: 'logement', params: {id: location.id}}">
+    <img :src="location.cover" :alt="location.title" class="thumb-image">
+    <p class="thumb-title">{{ location.title }}</p>
+  </router-link>
 </template>
---
+++ src/views/LogementView.vue
@@ -0,0 +1,13 @@
+<script>
+import Header from "../components/Header.vue"
+  export default {
+    name: "Logement",
+    components: {
+      Header
+    }
+  }
+</script>
+
+<template>
+  <Header />
+</template>
