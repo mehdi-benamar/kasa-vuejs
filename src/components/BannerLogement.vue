005012f77f9e3f0f95e51a5b11783e759cc1230b
Mehdi Benamar <mistahbe213@gmail.com>
2022-12-12 11:12:34 +0100

    add arrow svg + create component banner logement

---
+++ src/components/BannerLogement.vue
@@ -0,0 +1,21 @@
+<script>
+import LeftArrow from "./icons/IconLeftArrow.vue"
+import RightArrow from "./icons/IconRightArrow.vue"
+
+export default {
+  name: "BannerLogement",
+  components: {
+    LeftArrow,
+    RightArrow
+  }
+}
+</script>
+
+<template>
+    <section class="bannerLog">
+      <img src="https://picsum.photos/200/300" alt="" class="bannerLog-image">
+      <small class="bannerLog-total">1/4</small>
+      <LeftArrow />
+      <RightArrow />
+    </section>
+</template>
--- src/views/LogementView.vue
+++ src/views/LogementView.vue
@@ -1,13 +1,17 @@
 <script>
-import Header from "../components/Header.vue"
+  import Header from "../components/Header.vue"
+  import BannerLogement from "../components/bannerLogement.vue"
+
   export default {
     name: "Logement",
     components: {
-      Header
+      Header,
+      BannerLogement
     }
   }
 </script>
 
 <template>
   <Header />
+  <BannerLogement />
 </template>
