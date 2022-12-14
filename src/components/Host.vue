7be5aea201921eccb5d9e40e16421d033005fa03
Mehdi Benamar <mistahbe213@gmail.com>
2022-12-14 14:36:02 +0100

    formatting css for host component

--- src/components/Host.vue
+++ src/components/Host.vue
@@ -21,22 +21,27 @@ export default {
       <div class="host-info">
         <h2>Title</h2>
         <h3>Location</h3>
-        <div class="host-tags">
-          <Tag name="test"/>
-          <Tag name="test"/>
-          <Tag name="test"/>
-        </div>
       </div>
       <div class="host-profile">
-        <p class="host-profile-name">Prénom<br>Nom</p>
-        <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg" alt="" class="host-profile-image">
-        <div class="host-profile-rating">
-          <Rating />
-          <Rating />
-          <Rating />
-          <Rating />
-          <Rating />
+        <div class="host-profile-name">
+          <p>Prénom</p>
+          <p>Nom</p>
         </div>
+        <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg" alt="" class="host-profile-image">
+      </div>
+    </section>
+    <section class="host-bottom">
+      <div class="host-bottom-tags">
+        <Tag name="test"/>
+        <Tag name="test"/>
+        <Tag name="test"/>
+      </div>
+      <div class="host-bottom-rating">
+        <Rating />
+        <Rating />
+        <Rating />
+        <Rating />
+        <Rating />
       </div>
     </section>
 </template>
